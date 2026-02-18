c1f870d72df517bb6668b78d46137a6da0f8ac5c
Rohit Vidhate <rohitvidhate3@gmail.com>
2026-02-18 11:58:39 +0530

    vercel

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     host: true
   },
-  base : process.env.VITE_BASE_PATH || "/MyPortfolio",
+  preview: {
+    port: 3001,
+    host: true
+  },
+  base: process.env.VITE_BASE_PATH || "/MyPortfolio",
 })
